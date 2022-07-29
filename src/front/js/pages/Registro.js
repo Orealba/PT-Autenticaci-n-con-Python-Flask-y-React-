c6ffa90b0c81f5dd3cd212a43652b92b8ed9dedb
Orealba <orealbas@gmail.com>
2022-07-29 16:52:35 +0000

    cambios home

---
+++ src/front/js/pages/Registro.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import Formulario from "../component/Formulario";
+
+export const Registro = () => {
+  return (
+    <div>
+      <Formulario />
+    </div>
+  );
+};
